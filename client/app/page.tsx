1ccac1dbf36fb571e6cdf85d219cfd52929ec158
soheila-entezar <166860318+soheila-entezar@users.noreply.github.com>
2025-01-09 16:21:41 +0330

    Revert "created the chart"
    This reverts commit 02fc8ec87229cc613938f765353ff8c4776e6c16.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,16 +7,14 @@ export default function Home() {
   return (
     <>
       <div className="h-screen   flex justify-center items-center">
-        <div className="w-[90%] md:w-[50%] min-h-[300px]">
+        <div className="w-[50%] min-h-[300px]">
           {/* //first row */}
-          <div className=" min-h-[100px] flex flex-wrap justify-between ">
-            <div className="w-[100%] md:w-[47%] max-h-[100px] mt-2 border border-[#e4e4e7]  rounded-md  ">
-              <SuccessChart />
-            </div>
-            <AddToDO />
-          </div>
+          <div className=" min-h-[100px] flex justify-between ">
+      <div className="w-[47%] max-h-[100px] border border-[#e4e4e7]  rounded-md  "><SuccessChart/></div>
+     <AddToDO/>
+    </div>
           {/* //second row */}
-          <ToDoList />
+          <ToDoList/>
         </div>
       </div>
     </>
--- client/components/AddToDo/AddToDO.tsx
+++ client/components/AddToDo/AddToDO.tsx
@@ -18,7 +18,7 @@ function AddToDO() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="w-[100%] md:w-[47%] min-h-[100px] mt-2 border border-[#e4e4e7]  flex flex-wrap justify-around items-center rounded-md">
+    <form onSubmit={handleSubmit} className=" w-[47%]  border border-[#e4e4e7]  flex flex-wrap justify-around items-center rounded-md">
       <div className="w-[90%] h-[20%] flex justify-end text-[13px] font-semibold">افزودن تسک جدید </div>
       <button type="submit" className="w-[30%] h-[35%] text-[10px] bg-[#4338ca] text-white rounded-md flex justify-center items-center ">
         افزودن
